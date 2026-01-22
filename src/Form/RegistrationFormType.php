48f9d4efaedf3ad86ef7dfe2dff8b89b7b021965
DenZaiyy <denzaiyy@outlook.fr>
2026-01-23 00:33:27 +0100

    fix: avatar instead of avatarFile (vich uploader)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -79,7 +79,7 @@ class RegistrationFormType extends AbstractType
                     )
                 ],
             ])
-            ->add('avatarFile', DropzoneType::class, [
+            ->add('avatar', DropzoneType::class, [
                 'label' => 'user.registration.avatar.label',
                 'attr' => [
                     'placeholder' => 'user.registration.avatar.placeholder',
