5bac5be22e6c4bb40c8475dbf2f1762b1264c7dd
DenZaiyy <denzaiyy@outlook.fr>
2026-02-01 01:19:05 +0100

    feat: twig filter to have formatted price with currency

---
+++ src/Twig/Extension/CurrencyExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Twig\Extension;
+
+use App\Twig\Runtime\CurrencyExtensionRuntime;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class CurrencyExtension extends AbstractExtension
+{
+    #[\Override]
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('currency', [CurrencyExtensionRuntime::class, 'getCurrency']),
+        ];
+    }
+}
