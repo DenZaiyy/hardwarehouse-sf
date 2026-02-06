2a8db34aafc7f3f9930910ed7ba866cf9be4f9ec
DenZaiyy <denzaiyy@outlook.fr>
2026-02-06 16:56:04 +0100

    fix: php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,11 +3,15 @@
 $finder = new PhpCsFixer\Finder()
     ->in(__DIR__)
     ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('tests')
 ;
 
 return new PhpCsFixer\Config()
     ->setRules([
         '@Symfony' => true,
+        // Désactiver la conversion des @var en commentaires simples
+        'phpdoc_to_comment' => false,
     ])
     ->setFinder($finder)
 ;
