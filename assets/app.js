e774bd95cc7d41d6291fb16a27af46d7706415a5
DenZaiyy <denzaiyy@outlook.fr>
2026-03-11 23:15:50 +0100

    feat: ordreAddress entity with repository

--- assets/app.js
+++ assets/app.js
@@ -3,19 +3,21 @@ import './styles/app.css';
 
 const button = document.getElementById('goTop');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-        button.classList.remove('opacity-0', 'invisible', 'translate-y-4');
-        button.classList.add('opacity-100', 'visible', 'translate-y-0');
-    } else {
-        button.classList.add('opacity-0', 'invisible', 'translate-y-4');
-        button.classList.remove('opacity-100', 'visible', 'translate-y-0');
-    }
-});
-
-button.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+if (button) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            button.classList.remove('opacity-0', 'invisible', 'translate-y-4');
+            button.classList.add('opacity-100', 'visible', 'translate-y-0');
+        } else {
+            button.classList.add('opacity-0', 'invisible', 'translate-y-4');
+            button.classList.remove('opacity-100', 'visible', 'translate-y-0');
+        }
     });
-});
+
+    button.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
